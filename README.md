8ac7e1c7b79bec7fa8d351d1ffecede597723975
Hemslo Wang <hemslo.wang@gmail.com>
2024-01-12 01:37:09 +1100

    Setup basic openai chat
    Signed-off-by: Hemslo Wang <hemslo.wang@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# chat-search
+
+## Installation
+
+```bash
+poetry install
+```
+
+## Launch LangServe
+
+```bash
+langchain serve
+```
+
+## Running in Docker
+
+```shell
+docker compose up --build
+```
---
+++ app/server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+load_dotenv()
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai",
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
