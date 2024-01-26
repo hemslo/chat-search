4d91cf0703e43c423645ae5e564db410644a4462
Hemslo Wang <hemslo.wang@gmail.com>
2024-01-26 19:18:39 +1100

    Add ingest in crawl
    Signed-off-by: Hemslo Wang <hemslo.wang@gmail.com>

--- app/client/crawl.py
+++ app/client/crawl.py
@@ -1,40 +1,99 @@
 #!/usr/bin/env python
 import argparse
+import asyncio
+import os
+from pprint import pprint
 
+from aiohttp import TCPConnector, ClientSession
 from bs4 import BeautifulSoup
 from langchain_community.document_loaders import SitemapLoader
+from langchain_core.documents import Document
+
+from app.models.web_document import WebDocumentMetadata
 
 
-def meta_function(meta: dict, soup: BeautifulSoup) -> dict:
+def meta_function(meta: dict, soup: BeautifulSoup) -> WebDocumentMetadata:
     title = soup.find("title")
     description = soup.find("meta", attrs={"name": "description"})
     html = soup.find("html")
     return {
         "source": meta["loc"],
         "title": title.get_text() if title else "",
-        "description": description.get("content", "") if description else "",
-        "language": html.get("lang", "") if html else "",
-        **meta,
+        "description": str(description.get("content", "")) if description else "",
+        "language": str(html.get("lang", "")) if html else "",
     }
 
 
-def crawl(sitemap_url: str):
+async def ingest_doc(
+    doc: Document,
+    ingest_url: str,
+    session: ClientSession,
+) -> tuple[str, bool]:
+    async with session.post(ingest_url, json=doc.dict()) as response:
+        return doc.metadata["source"], response.ok
+
+
+async def ingest(
+    docs: list[Document],
+    ingest_url: str,
+    ingest_concurrency: int,
+) -> dict[str, bool]:
+    connector = TCPConnector(limit=ingest_concurrency)
+    async with ClientSession(connector=connector) as session:
+        results = await asyncio.gather(
+            *(ingest_doc(doc, ingest_url, session) for doc in docs),
+        )
+        return dict(results)
+
+
+def crawl(
+    sitemap_url: str,
+    crawl_concurrency: int,
+    ingest_url: str,
+    ingest_concurrency: int,
+) -> None:
     docs = SitemapLoader(
         sitemap_url,
         meta_function=meta_function,
+        requests_per_second=crawl_concurrency,
     ).load()
-    return docs
+
+    result = asyncio.run(ingest(docs, ingest_url, ingest_concurrency))
+
+    pprint(result, sort_dicts=True)
+    failed_urls = [url for url, ok in result.items() if not ok]
+    if failed_urls:
+        raise Exception(f"Failed to ingest {len(failed_urls)} URLs: {failed_urls}")
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description="Crawl.")
     parser.add_argument(
         "--sitemap-url",
         type=str,
         help="The URL of the sitemap to crawl.",
+        required=True,
+    )
+    parser.add_argument(
+        "--crawl-concurrency",
+        type=int,
+        default=os.environ.get("CRAWL_CONCURRENCY", 10),
+        help="The number of concurrent crawl requests to make.",
+    )
+    parser.add_argument(
+        "--ingest-url",
+        type=str,
+        default=os.environ.get("INGEST_URL", "http://localhost:8000/ingest"),
+        help="The URL of the ingest endpoint.",
+    )
+    parser.add_argument(
+        "--ingest-concurrency",
+        type=int,
+        default=os.environ.get("INGEST_CONCURRENCY", 10),
+        help="The number of concurrent ingest requests to make.",
     )
     args = parser.parse_args()
-    crawl(args.sitemap_url)
+    crawl(**vars(args))
 
 
 if __name__ == "__main__":
--- app/routers/ingest.py
+++ app/routers/ingest.py
@@ -1,45 +1,13 @@
-import hashlib
 import os
 from pathlib import Path
-from typing import TypedDict
-from urllib.parse import urlparse, urlunparse
 
 from fastapi import APIRouter
 from langchain_community.vectorstores.redis import Redis
-from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
-from pydantic import Field
+
+from ..models.web_document import WebDocument
 
 embeddings = OpenAIEmbeddings()
-
-
-class WebDocumentMetadata(TypedDict):
-    source: str
-    title: str
-    description: str
-    language: str
-
-
-class WebDocument(Document):
-    metadata: WebDocumentMetadata = Field(default_factory=WebDocumentMetadata)
-
-    def source_id(self):
-        source = self.normalize_url(self.metadata["source"])
-        return hashlib.sha256(source.encode()).hexdigest()
-
-    @staticmethod
-    def normalize_url(url) -> str:
-        parsed_url = urlparse(url)
-        return urlunparse(
-            (
-                parsed_url.scheme,
-                parsed_url.netloc,
-                parsed_url.path,
-                parsed_url.params,
-                parsed_url.query,
-                "",
-            )
-        )
 
 
 router = APIRouter()
