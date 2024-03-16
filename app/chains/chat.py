bca1c3f3e79fc50e534b2ee5490097c2e92cf0f0
Hemslo Wang <hemslo.wang@gmail.com>
2024-03-17 01:00:54 +1100

    Update dependencies
    Signed-off-by: Hemslo Wang <hemslo.wang@gmail.com>

--- app/chains/chat.py
+++ app/chains/chat.py
@@ -1,7 +1,8 @@
 from collections.abc import Sequence
 
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.documents import Document
 from langchain_core.messages import (
     HumanMessage,
